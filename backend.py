c83c85a61e043ab0ad8f0034423aeb3d3940b874
Caitlin Senior <128043930+CSenior-01@users.noreply.github.com>
2023-10-16 19:25:04 +0100

    Add files via upload

---
+++ backend.py
@@ -0,0 +1,197 @@
+# Caitlin Senior
+# Python Smart Home coursework - backend
+# up2113762
+
+
+class SmartPlug:
+    """The settings and functions for the smart plug device"""
+
+    def __init__(self):
+        """Initialise the default settings for the smart plug"""
+        self.switchedOn = False
+        self.consumptionRate = 0
+
+    def toggleSwitch(self, newSwitch):
+        """Turning the smart plug switch on or off"""
+        if newSwitch == True:
+            self.switchedOn = not self.switchedOn
+        elif newSwitch == False:
+            self.switchedOn = not self.switchedOn
+
+    def getSwitch(self):
+        """Return the current state of the smart plug switch"""
+        return self.switchedOn
+
+    def getConsumption(self):
+        """Return the current consumption rate of the smart plug"""
+        return self.consumptionRate
+
+    def setConsumption(self, newConsumption):
+        """Setting a new consumption rate for the smart plug"""
+        if newConsumption >= 0 and newConsumption <= 150:
+            self.consumptionRate = newConsumption
+            return True
+        else:
+            print("Value must be between 0 and 150!")
+            return False
+
+    def __str__(self):
+        """Return a formatted string representation of the smart plug"""
+        if self.switchedOn == True:
+            return "Your Smart Plug is {} and the consumption rate is {}".format("on", self.consumptionRate)
+        elif self.switchedOn == False:
+            return "Your Smart Plug is {} and the consumption rate is {}".format("off", self.consumptionRate)
+
+
+class SmartHeater(SmartPlug):
+    """The settings and functions for the smart heater device"""
+
+    def __init__(self):
+        """Initialise the default settings for the smart heater, inherits from SmartPlug"""
+        super().__init__()
+        self.setting = 0
+
+    def toggleSwitch(self, newSwitch):
+        """Turning the smart heater switch on or off"""
+        if newSwitch == True:
+            self.switchedOn = not self.switchedOn
+        elif newSwitch == False:
+            self.switchedOn = not self.switchedOn
+
+    def getSwitch(self):
+        """Return the current state of the smart heater switch"""
+        return self.switchedOn
+
+    def getSetting(self):
+        """Return the current setting of the smart heater"""
+        return self.setting
+
+    def changeSetting(self, newSetting):
+        """Changing the setting of the smart heater"""
+        if newSetting >= 0 and newSetting <= 5:
+            self.setting = newSetting
+            return True
+        else:
+            print("Value must be between 0 and 5!")
+            return False
+
+    def __str__(self):
+        """Return a formatted string representation of the smart heater"""
+        if self.switchedOn == True:
+            return "Your Smart Heater is {} and the setting is {}".format("on", self.setting)
+        elif self.switchedOn == False:
+            return "Your Smart Heater is {} and the setting is {}".format("off", self.setting)
+
+class SmartHome:
+    """The functions of the smart home to manage and control the devices"""
+
+    def __init__(self):
+        """Create an empty list of devices for the smart home"""
+        self.devices = []
+
+    def getDevices(self):
+        """Return the items stored in the list of devices"""
+        return self.devices
+
+    def getDevicesAt(self, index):
+        """Take a value and return the device at that index within the device list"""
+        return self.devices[index]
+
+    def addDevice(self, device):
+        """Add a new device to the list of devices"""
+        self.devices.append(device)
+
+    def toggleSwitch(self, index):
+        """Turns a specific device in the device list on or off"""
+        if index < len(self.devices):
+            device = self.devices[index]
+            if device.switchedOn == False:
+                device.toggleSwitch(True)
+                return device.switchedOn
+            if device.switchedOn == True:
+                device.toggleSwitch(False)
+                return device.switchedOn
+
+    def turnOnAll(self):
+        """Turns on all devices in the list"""
+        for device in self.devices:
+            if device.switchedOn == False:
+                device.switchedOn = True
+            else:
+                pass
+
+    def turnOffAll(self):
+        """Turns off all devices in the list"""
+        for device in self.devices:
+            if device.switchedOn == True:
+                device.switchedOn = False
+            else:
+                pass
+
+    def __str__(self):
+        """Return a formatted string representation of all the devices in the device list"""
+        output = "Current devices: \n"
+        for device in self.devices:
+            device = str(device)
+            output += "{} \n".format(device)
+        return output
+
+
+def testSmartPlug():
+    """Tests the functions of the smart plug class"""
+
+    plug = SmartPlug()
+    plug.toggleSwitch(True)
+    if plug.getSwitch() == True:
+        print("The switch is {}".format("on"))
+    else:
+        print("The switch is {}".format("off"))
+    print("The current consumption rate is {}".format(plug.getConsumption()))
+    while True:
+        if (plug.setConsumption(int(input("What should the new value be? ")))) == False:
+            pass
+        else:
+            print("The current consumption rate is {}".format(plug.getConsumption()))
+            print(plug)
+            break
+
+# testSmartPlug()
+
+def testDevice():
+    """Tests the functions of the smart heater class"""
+
+    heater = SmartHeater()
+    heater.toggleSwitch(True)
+    if heater.getSwitch() == True:
+        print("The switch is {}".format("on"))
+    else:
+        print("The switch is {}".format("off"))
+    print("The current setting is {}".format(heater.getSetting()))
+    while True:
+        if heater.changeSetting(int(input("What should the new setting be? "))) == False:
+            pass
+        else:
+            print("The current setting is {}".format(heater.getSetting()))
+            print(heater)
+            break
+
+# testDevice()
+
+def testSmartHome():
+    """Tests the functions of the smart home class"""
+
+    home = SmartHome()
+    plug1 = SmartPlug()
+    plug2 = SmartPlug()
+    heater = SmartHeater()
+    plug2.toggleSwitch(True)
+    plug2.setConsumption(45)
+    heater.changeSetting(3)
+    home.addDevice(plug1)
+    home.addDevice(plug2)
+    home.addDevice(heater)
+    print(home)
+    home.turnOnAll()
+    print(home)
+
+# testSmartHome()
---
+++ frontend.py
@@ -0,0 +1,148 @@
+# Caitlin Senior
+# up2113762
+# Python Smart Home coursework - frontend
+
+from backend import *
+from tkinter import *
+
+mainWin = Tk()
+home = SmartHome()
+
+def setUpHome():
+    """Sets up the list of devices in the smart home"""
+    homeDevices = [SmartHeater(), SmartHeater(), SmartHeater(), SmartPlug(), SmartPlug()]
+    for i in range(len(homeDevices)):
+        home.addDevice(homeDevices[i])
+
+def setUpMainWin():
+    """Sets up the main window of the smart home GUI"""
+    mainWin.title("Smart Home")
+    devicesNum = len(home.devices)
+    height = 50 * (devicesNum + 2)
+    mainWin.geometry("600x{}".format(height))
+    mainWin.resizable(False, False)
+
+    mainWin.columnconfigure(index=0, weight=4)
+
+    showDevices()
+
+    mainWin.mainloop()
+
+def showDevices():
+    """Sets up the devices of the GUI"""
+    devicesNum = len(home.devices)
+
+    def turnAllOff():
+        home.turnOffAll()
+        updateLabels()
+
+    def turnAllOn():
+        home.turnOnAll()
+        updateLabels()
+
+    turnAllOffBtn = Button(mainWin, text="Turn all off", command=turnAllOff)
+    turnAllOffBtn.grid(row=1, column=0, padx=10, pady=5, sticky="w")
+
+    turnAllOnBtn = Button(mainWin, text="Turn all on", command=turnAllOn)
+    turnAllOnBtn.grid(row=2, column=0, padx=10, pady=5, sticky="w")
+
+    for deviceIndex in range(devicesNum):
+        smartDevice = home.getDevicesAt(deviceIndex)
+
+        deviceTxt = Text(mainWin, height=2, width=50)
+        deviceTxt.insert("1.0", str(smartDevice))
+        deviceTxt.grid(row=deviceIndex + 3, column=0, padx=10, pady=5)
+
+        def updateLabels():
+            showDevices()
+
+        def toggleThis(i=deviceIndex):
+            home.toggleSwitch(i)
+            updateLabels()
+
+        def devicesOn(i = deviceIndex):
+            numDevicesOn = 0
+            gettingDevice = home.getDevicesAt(i)
+            onStatus = gettingDevice.getSwitch()
+            for device in range(len(home.devices)):
+                if onStatus == True:
+                    numDevicesOn += 1
+                else:
+                    pass
+            return numDevicesOn
+
+        def modifyThis(i = deviceIndex):
+            modifyWindow(i)
+
+        toggleBtn = Button(mainWin, text="Toggle This", command=toggleThis)
+        toggleBtn.grid(row=deviceIndex + 3, column=2, padx=10, pady=5)
+
+        configBtn = Button(mainWin, text="Modify", command=modifyThis)
+        configBtn.grid(row=deviceIndex + 3, column=1, padx=10, pady=5)
+
+        devicesOn = Label(mainWin, text="Total devices on: {}".format(devicesOn()), height=2, width=50)
+        devicesOn.grid(row=deviceIndex + 4, column=0, padx=10, pady=5, sticky="w")
+
+def modifyWindow(deviceIndex):
+    """Opens an additional window to modify the devices"""
+
+    modifyWin = Toplevel(mainWin)
+    modifyWin.geometry("400x150")
+    modifyWin.resizable(False, False)
+
+    deviceName = home.getDevicesAt(deviceIndex)
+    deviceClass = deviceName.__class__.__name__
+
+    modifyWin.title("Modify {}".format(deviceClass))
+
+    infoTxt = Text(modifyWin, height=2, width=45)
+
+    if deviceClass == "SmartHeater":
+        infoTxt.insert("1.0", "Modify your Smart Heater")
+        settingHeaterLabel = Label(modifyWin, text="Change the heater setting:")
+        settingHeaterLabel.grid(row=1, column=0, padx=10, pady=10)
+
+        settingEntry = Entry(modifyWin)
+        settingEntry.insert(0, str(deviceName.getSetting()))
+        settingEntry.grid(row=1, column=1, padx=10, pady=10)
+
+        def submitCmd():
+            newSetting = int(settingEntry.get())
+            deviceName.changeSetting(newSetting)
+            showDevices()
+            modifyWin.destroy()
+
+        submitBtn = Button(modifyWin, text="Submit", command=submitCmd)
+        submitBtn.grid(row=2, column=1, padx=10, pady=2)
+
+    elif deviceClass == "SmartPlug":
+        infoTxt.insert("1.0", "Modify your Smart Plug")
+        settingConsumptionLabel = Label(modifyWin, text="Change the consumption rate:")
+        settingConsumptionLabel.grid(row=1, column=0, padx=10, pady=10)
+
+        consumptionEntry = Entry(modifyWin)
+        consumptionEntry.insert(0, str(deviceName.getConsumption()))
+        consumptionEntry.grid(row=1, column=1, padx=10, pady=10)
+
+        def submitCmd():
+            newConsumption = int(consumptionEntry.get())
+            deviceName.setConsumption(newConsumption)
+            showDevices()
+            modifyWin.destroy()
+
+        confirmBtn = Button(modifyWin, text="Confirm", command=submitCmd)
+        confirmBtn.grid(row=2, column=1, padx=10, pady=2)
+
+    infoTxt.grid(row=0, column=0, padx=15, pady=5, columnspan=2)
+
+    cancelBtn = Button(modifyWin, text="Cancel", command=modifyWin.destroy)
+    cancelBtn.grid(row=2, column=0, padx=10, pady=10)
+
+    modifyWin.mainloop()
+
+def main():
+    """Runs the main functions"""
+    setUpHome()
+    setUpMainWin()
+
+main()
